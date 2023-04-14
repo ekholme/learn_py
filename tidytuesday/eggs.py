91332057b2e300a141c1d9b20d02cdd36359ebe7
Eric Ekholm <eric.ekholm@gmail.com>
2023-04-14 16:30:02 -0400

    some more fiddling

--- tidytuesday/eggs.py
+++ tidytuesday/eggs.py
@@ -17,3 +17,21 @@ eggs_df.shape
 np.corrcoef(eggs_df['n_hens'], eggs_df['n_eggs'])
 #ok so these are incredibly highly correlated
 
+#let's get the type of the columns
+eggs_df.dtypes
+
+#ok, let's parse the observed_month to a date
+eggs_df['date'] = pd.to_datetime(eggs_df['observed_month'], format='%Y-%m-%d')
+
+#confirm this worked
+eggs_df.dtypes
+#ok cool
+
+#and what if we want to get the month from each date?
+eggs_df['month'] = eggs_df['date'].dt.month
+eggs_df['year'] = eggs_df['date'].dt.year
+
+#and let's get the avg eggs per month
+month_mean = eggs_df.groupby('month')['n_eggs'].mean().reset_index()
+
+month_mean
